9ca415b6c9ab329c932bc303eb2470d3d12e4925
Clay2605293 <153925795+Clay2605293@users.noreply.github.com>
2024-05-23 09:35:45 -0600

    Fixed Express Game

--- expressGame.h
+++ expressGame.h
@@ -13,97 +13,76 @@
 void expressGame() {
     Tablero newGame;
     Dice newDice;
-    int turn=1;
-    int playingPlayer=1;
-    int numberOfPlayers=2;
+    int turn = 1;
+    int playingPlayer = 1;
+    int numberOfPlayers = 2;
     char option;
-    string name1,name2;
-    int playerNumber1=1;
-    int playerNumber2=2;
+    string name1, name2;
 
-    cout<<"In this game the L board squares will increase position by 3, S board squares will decrease position by 3."<<endl;
-    cout<<"As soon as a player reaches 100 or over, wins."<<endl;
+    cout << "In this game the L board squares will increase position by 3, S board squares will decrease position by 3." << endl;
+    cout << "As soon as a player reaches 100 or over, wins." << endl;
 
-    Player player1=Player(1);
-    Player player2=Player(2);
+    Player player1 = Player(1);
+    Player player2 = Player(2);
     player1.setPosition(1);
     player2.setPosition(1);
-    cout<<"////////////////////////////////////////////////"<<endl;
+    cout << "////////////////////////////////////////////////" << endl;
 
-    while (player1.getPosition()<=99 or player1.getPosition()<=99){
-        cout<<player1.getName()<<" is in position: "<<player1.getPosition()<<endl;
-        cout<<player2.getName()<<" is in position: "<<player2.getPosition()<<endl;
-        newGame.printTablero(player1.getPosition(),player2.getPosition());
-        if (playingPlayer==1){
-            cout<<"Turn "<<turn<<endl;
-            cout<<"It's "<<player1.getName()<<"'s turn"<<endl;
-            cout << "Choose an option"<< endl;
-            cout << "R roll dice" << endl;
-            cout << "E exit game" << endl;
-            cout << "Enter selection: ";
-            cin >> option;
-            option = tolower(option);
-            switch (option) {
-                case 'r':{
-                    int roll=newDice.diceRoll();
-                    int actualpos=player1.getPosition();
-                    int newpos=actualpos+roll;
-                    cout << "The dice rolled: " << roll <<endl;
-                    cout<<player1.getName()<<" goes to square "<<newpos<<endl;
-                    player1.setPosition(newpos);
-                    turn++;
-                    break;
-                }
-                case 'e':{
-                    break;
-                }
-                default:
-                    cout << "Please choose a valid option." << endl;
-                    break;
-            }
+    while (player1.getPosition() < 100 && player2.getPosition() < 100) {
+        cout << player1.getName() << " is in position: " << player1.getPosition() << endl;
+        cout << player2.getName() << " is in position: " << player2.getPosition() << endl;
+        newGame.printTablero(player1.getPosition(), player2.getPosition());
+
+        cout << "Turn " << turn << endl;
+        if (playingPlayer == 1) {
+            cout << "It's " << player1.getName() << "'s turn" << endl;
+        } else {
+            cout << "It's " << player2.getName() << "'s turn" << endl;
         }
 
-        if (playingPlayer==2){
-            cout<<"Turn "<<turn<<endl;
-            cout<<"It's "<<player2.getName()<<"'s turn"<<endl;
-            cout << "Choose an option"<< endl;
-            cout << "R roll dice" << endl;
-            cout << "E exit game" << endl;
-            cout << "Enter selection: ";
-            cin >> option;
-            option = tolower(option);
-            switch (option) {
-                case 'r':{
-                    int roll=newDice.diceRoll();
-                    int actualpos=player2.getPosition();
-                    int newpos=actualpos+roll;
-                    cout << "The dice rolled: " << roll <<endl;
-                    cout<<player2.getName()<<" goes to square "<<newpos<<endl;
-                    player2.setPosition(newpos);
-                    turn++;
-                    break;
+        cout << "Choose an option" << endl;
+        cout << "R roll dice" << endl;
+        cout << "E exit game" << endl;
+        cout << "Enter selection: ";
+        cin >> option;
+        option = tolower(option);
+        int roll, actualPos, newPos;
+        switch (option) {
+            case 'r':
+                roll = newDice.diceRoll();
+                if (playingPlayer == 1) {
+                    actualPos = player1.getPosition();
+                    newPos = actualPos + roll;
+                    cout << "The dice rolled: " << roll << endl;
+                    cout << player1.getName() << " goes to square " << newPos << endl;
+                    player1.setPosition(newPos);
+                    playingPlayer = 2;
+                } else {
+                    actualPos = player2.getPosition();
+                    newPos = actualPos + roll;
+                    cout << "The dice rolled: " << roll << endl;
+                    cout << player2.getName() << " goes to square " << newPos << endl;
+                    player2.setPosition(newPos);
+                    playingPlayer = 1;
                 }
-                case 'e':{
-                    break;
-                }
-                default:
-                    cout << "Please choose a valid option." << endl;
-                    break;
-            }
-        }
-        if (playingPlayer==1){
-            playingPlayer=2;
-        }
-        if (playingPlayer==2){
-            playingPlayer=1;
+                turn++;
+                break;
+            case 'e':
+                cout << "Game exited." << endl;
+                return;
+            default:
+                cout << "Please choose a valid option." << endl;
+                break;
         }
 
-        cout<<"////////////////////////////////////////////////"<<endl;
+        cout << "////////////////////////////////////////////////" << endl;
     }
 
+    if (player1.getPosition() >= 100) {
+        cout << player1.getName() << " wins!" << endl;
+    } else if (player2.getPosition() >= 100) {
+        cout << player2.getName() << " wins!" << endl;
+    }
 }
-
-
-
 
 #endif //SERPIENTESESCALERAS_EXPRESSGAME_H

3859b1140297c1b3e4e9a4cea98b2ba7cf5e023b
emihdz1209 <emiliohdz35@gmail.com>
2024-05-23 10:10:33 -0600

    READ ME update

---
+++ cmake-build-debug/CMakeFiles/SerpientesEscaleras.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SerpientesEscaleras.dir/dice.cpp.obj"
+  "CMakeFiles/SerpientesEscaleras.dir/dice.cpp.obj.d"
+  "CMakeFiles/SerpientesEscaleras.dir/main.cpp.obj"
+  "CMakeFiles/SerpientesEscaleras.dir/main.cpp.obj.d"
+  "CMakeFiles/SerpientesEscaleras.dir/player.cpp.obj"
+  "CMakeFiles/SerpientesEscaleras.dir/player.cpp.obj.d"
+  "CMakeFiles/SerpientesEscaleras.dir/tablero.cpp.obj"
+  "CMakeFiles/SerpientesEscaleras.dir/tablero.cpp.obj.d"
+  "SerpientesEscaleras.exe"
+  "SerpientesEscaleras.exe.manifest"
+  "SerpientesEscaleras.pdb"
+  "libSerpientesEscaleras.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SerpientesEscaleras.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
